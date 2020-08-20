36b4f9f318c7216574967042ee57fae07d90409f
Manak Pandey <manakpandey@gmail.com>
2020-08-20 14:48:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+from script import host_up, scan_port, sniff_port
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", host_up=host_up())
+
+
+@app.route('/scan', methods=['GET'])
+def scan_ports():
+    min_p = request.args.get('minPort')
+    max_p = request.args.get('maxPort')
+
+    try:
+        min_p = int(min_p)
+        max_p = int(max_p)
+    except TypeError:
+        return jsonify({"Error": "Invalid"})
+
+    if min_p < 0 or max_p < 0 or max_p < min_p:
+        return jsonify({"Error": "Invalid"})
+
+    open_ports = []
+    if host_up():
+        for p in range(int(min_p), int(max_p) + 1):
+            port_open = scan_port(p)
+            if port_open:
+                open_ports.append(p)
+
+    return jsonify(open_ports)
+
+
+@app.route('/sniff', methods=['GET'])
+def sniff():
+    port = request.args.get('port')
+    if not port:
+        return jsonify({"Error": "Invalid"})
+    packets = sniff_port(port, count=5, timeout=1)
+    return jsonify(list(packets))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ script.py
@@ -0,0 +1,91 @@
+from logging import getLogger, ERROR
+from scapy.all import *
+import sys
+from datetime import datetime
+from time import strftime
+
+getLogger("scapy.runtime").setLevel(ERROR)
+
+SYN_ACK = 0x12
+RST_ACK = 0x14
+
+verbose = 1
+
+if len(sys.argv) > 1 and sys.argv[1] == 'v':
+    verbose = 1
+
+
+def exit(n):
+    print("\n[!] Exiting gracefully...")
+    sys.exit(n)
+
+
+def host_up(ip="127.0.0.1"):
+    conf.verb = verbose
+    conf.L3socket = L3RawSocket
+    try:
+        ping = sr1(IP(dst=ip) / ICMP())
+        print("[*] Target is UP")
+        return True
+    except Exception:
+        print("[*] Couldn't resolve host.")
+        return False
+
+
+def scan_port(port, target="127.0.0.1"):
+    src_port = RandShort()
+    try:
+        conf.verb = verbose
+        conf.L3socket = L3RawSocket
+        SYN_ACK_packet = sr1(IP(dst=target) / TCP(sport=src_port, dport=port, flags='S'))
+        pkt_flags = SYN_ACK_packet.getlayer(TCP).flags
+        if pkt_flags == SYN_ACK:
+            return True
+        return False
+    except KeyboardInterrupt:
+        RST_pkt = IP(dst=target) / TCP(sport=src_port, dport=port, flags="R")  # Built RST packet
+        send(RST_pkt)
+        return False
+    # RST sent by default
+
+
+def sniff_port(port, count, timeout=1):
+    return sniff(filter='port {}'.format(port), count=count, timeout=timeout)
+
+#
+# try:
+#     target = "127.0.0.1"
+#     min_port = input("[*] Enter Minimum Port Number: ")
+#     max_port = input("[*] Enter Maximum Port Number: ")
+#
+#     try:
+#         if not (0 <= int(min_port) <= int(max_port) and int(max_port) >= 0):
+#             print("\n[!] Invalid Range")
+#             exit(1)
+#
+#     except Exception:
+#         print("\n[!] Invalid Range")
+#         exit(1)
+#
+#     ports = range(int(min_port), int(max_port) + 1)
+#     start_clk = datetime.now()
+#
+#     host_up()
+#
+#     print("\n[*] Scanning started at {}\n".format(strftime("%H:%M:%S")))
+#     for p in ports:
+#         status = scan_port(target, p)
+#         if status:
+#             pkts = sniff(filter='port {}'.format(p), count=5, timeout=1)
+#             print('Port {}:  Open'.format(p))
+#             print(pkts)
+#
+#     stop_clk = datetime.now()
+#     total_time = stop_clk - start_clk
+#
+#     print("\n[*] Finished Scanning.")
+#     print("\n[*] Scanned {} ports in {}".format(len(ports), total_time))
+#
+# except KeyboardInterrupt:
+#     print("\n[*] Shutdown requested.")
+#     exit(1)
